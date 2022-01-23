9c4c5b7f18263bfc632634fd55c19e97b52d447f
sasikumar <sasikumar@Rev-PG02MX84>
2022-01-22 23:19:15 -0800

    Initial Commit

---
+++ src/test/java/com/revature/projects/shopper/ShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.projects.shopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
